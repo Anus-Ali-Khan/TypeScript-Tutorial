13503264fec97f8132ae240f264f76a9d7238c5c
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-08-10 11:09:41 +0500

    basic_Types

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,17 @@
 "use strict";
-let username = "Anas";
-console.log(username);
+let myName;
+let meaningOfLife;
+let isLoading;
+// let album: any;
+let album2; // Union type
+myName = "John";
+meaningOfLife = 42;
+isLoading = true;
+// album = 1984;
+album2 = 54;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
-let username = "Anas";
+let myName: string;
+let meaningOfLife: number;
+let isLoading: boolean;
+// let album: any;
+let album2: string | number; // Union type
 
-console.log(username);
+myName = "John";
+meaningOfLife = 42;
+isLoading = true;
+// album = 1984;
+album2 = 54;
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
