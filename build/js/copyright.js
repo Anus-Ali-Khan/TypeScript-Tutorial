aae1f2006f5df13e6121559ce976cebebb226d30
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-08-11 17:21:09 +0500

    typeScript with classes

--- build/js/copyright.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//Original JS code
-const year = document.getElementById("year");
-const thisYear = new Date().getFullYear();
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear;
